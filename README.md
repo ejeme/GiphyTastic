b24eab58d3d8870756b27853df4558e6c24fdc06
ejeme <tomijogun@yahoo.com>
2018-03-08 23:23:19 -0600

    Updated the giphytastic project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GiphyTastic
 Use Giphy API in javascript and jquery
+1. Create buttons based on elements in the array
+    When the submit button is clicked....
+        Get the value of the text box and add to the array
+        
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,11 @@ $(document).ready(function ()
     // Global Variables
     //==================================================================================================
     var topics = ['elephant', 'giraffe', 'hippopotamus', 'lemur', 'zebra', 'rhinoceros', 'green mamba', 'hynena', 'cheetah', 'lion', 'gorilla', 'antelope', 'baboon', 'buffalo', 'crocodile', 'jackal', 'leopard', 'mongoose', 'ostrich', 'monkey', 'wildebeest', 'warthog', 'pangolin', 'serval'];
-   //console.log(topics);
-    var stillImgUrl = '';
-    var animateImgUrl = '';
+   
+    var stillImg = '';
+    var animatedImg = '';
 
+    
     // Functions
     //==================================================================================================
     var createBtn = function () 
@@ -15,10 +16,12 @@ $(document).ready(function ()
         //removes all elements within the btn-section
         $('#btn-section').empty();
         //Create buttons based on elements in array
-        for (var i = 0; 1 < topics.length; i++) 
+        for (var i = 0; i < topics.length; i++) 
         {
             //Create new buttons
             var newBtn = $('<button>');
+
+    
             //Give button an attribute //COME BACK TO THIS******
             newBtn.attr('data-type', topics[i]);
             //Add class to button
@@ -28,8 +31,8 @@ $(document).ready(function ()
             //Add button to DOM
             $('#btn-section').append(newBtn);
         }
-console.log(createBtn);
-    }
+    }      
+
     var submit = function () 
     {
     //When submit button is clicked..........
@@ -48,34 +51,64 @@ console.log(createBtn);
     });
     }
 
-    var displayGif = function()
-    {
+    var displayGif = function(){
     //Gets the value of the button that is clicked
     var btnVal = $(this).data('type');
     // API Url and key
     var apiKey = 'WO5h5OqOs3Pb0iRpuBjAUQ6KN4uaAQ89';
-    var apiUrl = 'https://api.giphy.com/v1/gifs/search?q='+btnVal + '&api_key=' + apiKey;
-        $.ajax
-        ({
-            url: apiUrl,
-            method: 'GET'
-        }).done(function(response)
-            {
+    var apiUrl = 'http://api.giphy.com/v1/gifs/search?q='+ btnVal + '&api_key=' + apiKey + '&limit=5';
+    $.ajax({
+        url: apiUrl,
+        method: "GET"
+      }).done(function(response){
+               // $('.gifImg').empty;
                 for (var i = 0; i < 10; i++)
                     {
-                        stillIngUrl = response['data'][i]['images']['fixed_height_still']['url'];
-                        animateUrl = response['data'][i]['images']['fixed_height']['url'];
+                        stillImg = response['data'][i]['images']['fixed_height_still']['url'];
+                        animatedImage = response['data'][i]['images']['fixed_height']['url'];
                         //Assign image element to newImg variable
                         var newImg = $('<img>');
-                        //Give img element stillImgUrl, animated $ src attributes
-                        newImg.attr('data-still', stillImgUrl);
-                        newImg.attr('data-animate', animateImgUrl);
-                    };
+                        //Give img element stillImg, animated $ src attributes
+                        newImg.attr('data-still', stillImg);
+                        newImg.attr('data-animate', animatedImg);
+                        newImg.attr('src', stillImg);
+                        newImg.addClass('gifImage');
+                        //Add new images to the DOM
+                        $('.gifImg').append(newImg);
+                    }
                 //Testing
                 console.log('The button value is = ' + btnVal);
-                console.log('Still Image Url = ' + stillImgUrl);
-                console.log('Animated Image Url = ' + animateImgUrl);
+                console.log('Still Image Url = ' + stillImg);
+                console.log('Animated Image Url = ' + animateImg);
             });
     }
+    var gifAnimate = function(){
+        var gifCondition = $(this).data('type');
+        var stillUrl = $(this).data('still');
+        var animateUrl = $(this).data('animate');
+        if (gifCondition === 'still'){
+            //Changes the gif to an animated image by switching the url
+            $(this).attr('src', animateUrl);
+            //Switches the data type to animate
+            $(this).data('type', 'animate');
+            //Testing
+            console.log(gifCondition);
+        }
+        else if(gifCondition === 'animate'){
+            //Change src to still
+            $(this).attr('src', stillUrl);
+            $(this).data('type', 'still');
+            //Testing
+            console.log(true);
+        }
+    }
+
+//Main
+//=================================================================================
+createBtn();
+submit();
+$(document).on('click', '.gif', displayGif);
 
 });
+
+
